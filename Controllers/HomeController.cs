5e990e2bc6d3fe822ae53850bbe64316f4344e6c
TheoTechStack <130162778+TheoTechStack@users.noreply.github.com>
2024-07-21 10:33:35 +0200

    Add Product Context for Home Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Shopping_Micro_Services.Data;
 using Shopping_Micro_Services.Models;
 
 namespace Shopping_Micro_Services.Controllers;
@@ -15,7 +16,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return View(ProductContext.Products);
     }
 
     public IActionResult Privacy()
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Shopping_Micro_Services.Models
 {
     public class Product
     {
+        public int  Id { get; set; }
+        public string? Name { get; set; }    
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+        public string? ImageFile { get; set; }   
+        public decimal Price { get; set; }
         
     }
 }
